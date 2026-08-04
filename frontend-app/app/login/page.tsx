7fb334f7a3cd5965a99fe0fd0d3c2186a17fb882
Tinotenda Jecha <tinotendajecha@gmail.com>
2026-08-04 12:07:46 +0200

    Last First Commit

--- frontend-app/app/login/page.tsx
+++ frontend-app/app/login/page.tsx
@@ -1,6 +1,6 @@
 export default function login(){
     return(
-        <div class = 'flex mt-20 size-3xl bg-white px-10 text-green-900'>
+        <div >
             <h1>Please Sign in to proceed</h1>
             <p>This is my login page</p>
         </div>
